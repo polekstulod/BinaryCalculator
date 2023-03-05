6757d5c790e640de42bfb3c4a1d55a587d90ed85
James Paul Tulod <paultulod@pm.me>
2023-03-06 01:38:02 +0800

    Add initial code

--- BinaryCalculator/BinaryCalculator.cpp
+++ BinaryCalculator/BinaryCalculator.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string input;
+    int checker;
+    do
+    {
+        std::cout << "===========================================================\n";
+        std::cout << "=============== Welome to Binary Calculator ===============\n";
+        std::cout << "===========================================================\n\n";
+
+        std::cout << "1. Decimal to Binary\n";
+        std::cout << "2. Binary to Decimal\n";
+        std::cout << "3. Exit\n\n";
+
+        std::cout << "Enter a Number Option: ";
+        std::getline(std::cin, input);
+
+        std::stringstream stream(input);
+        if (stream >> checker) {
+            if (checker == 0)
+                std::cout << "Exited Successfully\n";
+            else if (checker == 1)
+                std::cout << "1. Decimal to Binary\n";
+            else if (checker == 2)
+                std::cout << "2. Binary to Decimal\n";
+        }
+        else {
+            std::cout << "Please Enter a valid number\n";
+            checker = 1;
+        }
+
+
+    } while (checker > 0);
+    
 }
 
